816431d4e9c842931e4ac01e9881d8b24e5302b7
UmbraSpace <jadenhunt76@gmail.com>
2025-03-01 03:13:31 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    //define new string variable
+    let output = "";
+    //for loop through string, decrementing index/counter
+    for(let i = input.length - 1; i >= 0; i--){
+        //append each character to new string
+        output += input.charAt(i);
+    }
+    //return string
+    return output;
 };
 
 // Do not edit below this line
