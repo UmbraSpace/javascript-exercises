4f6fd65b1c1abe43353ae768b2f5bc96ac9029b2
UmbraSpace <jadenhunt76@gmail.com>
2025-03-02 21:56:34 -0500

    Start exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    //for loop, set i equal to first argument and increment until greater than second
+    //for each iteration, add i to sum variable
+    //return sum
 
+    let sum = 0;
+
+    if (num2 > num1)
+        for (let i = num1; i <= num2; i++) sum+=i;
+    else
+        for (let i = num2; i <= num1; i++) sum+=i;
+
+    return sum;
 };
 
 // Do not edit below this line
