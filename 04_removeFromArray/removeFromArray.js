1ccf6907d0c9dacfe0c27d37b5f2b1a5934b2990
UmbraSpace <jadenhunt76@gmail.com>
2025-03-02 20:04:48 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...unwantedItems) {
+    //for loop through array
+    //for each array item, for loop through unwanted items
+    //if array item equals unwanted item, splice array item out of array and break out of inner loop
+    //return array
+    for (let i = 0; i < array.length; i++){
+        for (let j = 0; j < unwantedItems.length; j++){
+            if (array[i] === unwantedItems[j]){
+                array.splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
