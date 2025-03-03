d1815a91cdc4f6554bc72e47fc970e0b4af9357e
UmbraSpace <jadenhunt76@gmail.com>
2025-03-03 16:13:10 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,19 @@ const sumAll = function(num1, num2) {
     //for loop, set i equal to first argument and increment until greater than second
     //for each iteration, add i to sum variable
     //return sum
-
+    
+    let negativeInput = (num1 < 0 || num2 < 0);
+    
+    let areIntegers = 
+    (Math.floor (num1) === num1 &&
+    Math.floor (num2) === num2);
+    
+    let areNumbers = 
+    (typeof num1 === "number" && typeof num2 === "number");
+    
+    if (!areIntegers || !areNumbers || negativeInput) return "ERROR";
+    
+    
     let sum = 0;
 
     if (num2 > num1)
