e057fa1d35bb02ae9bfaeaa92af3171df61c7c6b
UmbraSpace <jadenhunt76@gmail.com>
2025-03-07 02:31:10 -0500

    Implement sum and multiply functions in calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,11 +7,20 @@ const subtract = function(num1, num2) {
 	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+  for (let num of nums){
+    sum += num;
+  }
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let product = 1;
+  for (let num of nums){
+    product *= num;
+  }
+  return product;
 
 };
 
