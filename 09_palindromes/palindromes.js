0b4ed6392ca1817b145e34abeb330e2bc0e336cc
UmbraSpace <jadenhunt76@gmail.com>
2025-03-09 21:27:08 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/ |\.|\,|\!/g, "");
+    string = string.toLowerCase();
+    
+    let reversed = string.split("").reverse().join("");
 
+    if (string === reversed) return true;
+    else return false;
 };
 
 // Do not edit below this line
