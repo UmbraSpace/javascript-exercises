6ebce0c805da912207b10db60f46874dd48dcde7
UmbraSpace <jadenhunt76@gmail.com>
2025-05-27 01:59:44 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,3 +1,15 @@
+function findTheOldest (array){
+    let oldest = array.reduce((acc, curr) => {
+        let accAge = acc.yearOfDeath ? acc.yearOfDeath - acc.yearOfBirth :
+        2025 - acc.yearOfBirth;
+        let currAge = curr.yearOfDeath ? curr.yearOfDeath - curr.yearOfBirth :
+        2025 - curr.yearOfBirth;
+        if (accAge >= currAge){
+            return acc;
+        } else return curr;
+    })
+    return oldest;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
